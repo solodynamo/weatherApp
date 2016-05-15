5d2a961a5aea17993d1bc4223207d202b807e376
akki <akki@localhost.localdomain>
2016-05-15 19:05:45 +0530

    got modified

--- code/main.js
+++ code/main.js
@@ -1,4 +1,5 @@
 var APPID="543c9b0117d84d90e32734d5f68585a9";
+//var APPID="d2ace650b4df41f6d5flea94fc08da75";
 var temp;
 var loc;
 var icon;
@@ -27,24 +28,78 @@ $(document).ready(function(){
     var weather={};
 
 
-   weather.loc='delhi';
+   /*weather.loc='delhi';
     weather.wind=5.6;
     weather.humidity='6%';
     weather.direction='R';
     weather.temp='45';
     weather.icon=21;
 
-    update(weather);
+    update(weather);*/
+
+    updateByZip(87110);
 
 })
 
+
+
+function updateByZip(zip)
+{
+    console.log("in updateByZip()");
+    var url="http://api.openweathermap.org/data/2.5/weather?"+"zip="+zip+"&APPID="+APPID;
+
+    console.log("url="+url);
+
+    request(url);
+
+}
+
+
+function request(url)
+{
+    console.log("in request()");
+    var xmlHttp=new XMLHttpRequest();
+
+ xmlHttp.onreadystatechange= function()
+ {
+     if(xmlHttp.readyState==4 && xmlHttp.status==200)
+         {
+             var weatherData=JSON.parse(xmlHttp.responseText);
+             var weather={};
+
+            console.log("in btw the request()"); weather.humidity=weatherData.main.humidity;
+             weather.windspeed=weatherData.wind.speed;
+             weather.direction=weatherData.wind.deg;
+             weather.location=weatherData.name;
+             weather.temp=weatherData.main.temp;
+             console.log(weather.direction);
+             console.log("hello");
+
+
+             update(weather);
+
+         }
+
+     else
+         console.log("unable to get data");
+
+ }
+
+ xmlHttp.open("GET",url,true);
+    xmlHttp.send();
+
+
+}
+
 function update(weather)
 {
+    console.log("in update()");
     //$(".windspeed").value=weather.wind;
-       $(".windspeed").append('<p>45 km/h</p>');
-    $(".direction").append('<p>n</p>');
-    $(".humidity").append('<p>45%</p>');
-    $(".location").append('<p>delhi</p>');
-$(".temperature").append('<p>46&deg</p>');
+       $(".windspeed").append(weather.windspeed);
+    $(".direction").append(weather.direction);
+    $(".humidity").append(weather.humidity);
+    $(".location").append(weather.location);
+$(".temperature").append(weather.temp);
     $(".climate_bg").src="images/"+weather.icon+"svg";
+
 }
